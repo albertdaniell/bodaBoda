592fd614cf19df319ccc75ba33fa7181326d9ece
alby <albertagoya@gmail.com>
2019-07-31 11:53:30 +0300

    Updating routes

--- nduthiApp/urls.py
+++ nduthiApp/urls.py
@@ -3,11 +3,15 @@ from nduthiApp import views
 
 urlpatterns = [
     path('api/rider/', views.rider_list),
-     path('api/owner/', views.owner_list),
-      path('api/vehicle/', views.vehicle_list),
-       path('api/insurance/', views.insurance_list),
-        path('api/sacco/', views.sacco_list),
+    path('api/owner/', views.owner_list),
+    path('api/vehicle/', views.vehicle_list),
+    path('api/insurance/', views.insurance_list),
+    path('api/sacco/', views.sacco_list),
     # path('api/restaurant_food/', views.restaurants_food),
     # path('api/restaurants/<int:pk>/', views.restaurant_detail),
-    
+    path('api/rider/<int:pk>/', views.rider_detail),
+    path('api/owner/<FrameNumber>/', views.owner_detail),
+    path('api/vehicle/<int:IDNo_id>/', views.vehicle_detail),
+    path('api/insurance/<int:pk>/', views.insurance_detail),
+    path('api/sacco/<int:IDNo_id>/', views.sacco_detail)
 ]
--- nduthiApp/views.py
+++ nduthiApp/views.py
@@ -110,3 +110,141 @@ def sacco_list(request):
             return JsonResponse(serializer.data, status=201)
             # return JsonResponse(data={'message': 'Hello, world!'})
         return JsonResponse(serializer.errors, status=400)
+
+
+
+# Details views
+
+
+@csrf_exempt
+def rider_detail(request, pk):
+    """
+    Retrieve, update or delete a code rider.
+    """
+    try:
+        riders = Rider.objects.get(pk=pk)
+    except Rider.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = RiderSerializer(riders)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = RiderSerializer(riders, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        riders.delete()
+        return HttpResponse(status=204)
+    
+    
+    
+@csrf_exempt
+def owner_detail(request, FrameNumber):
+    """
+    Retrieve, update or delete a code owner.
+    """
+    try:
+        owners = Owner.objects.get(FrameNumber=FrameNumber)
+    except Owner.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = OwnerSerializer(owners)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = OwnerSerializer(owners, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        owners.delete()
+        return HttpResponse(status=204)
+    
+
+@csrf_exempt
+def vehicle_detail(request, IDNo_id):
+    """
+    Retrieve, update or delete a vehicle.
+    """
+    try:
+        vehicle = Vehicle.objects.get(IDNo_id=IDNo_id)
+    except Vehicle.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = VehicleSerializer(vehicle)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = VehicleSerializer(vehicle, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        vehicle.delete()
+        return HttpResponse(status=204)
+    
+@csrf_exempt
+def insurance_detail(request, pk):
+    """
+    Retrieve, update or delete a code owner.
+    """
+    try:
+        insurance = Insurance.objects.get(pk=pk)
+    except Insurance.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = InsuranceSerializer(insurance)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = InsuranceSerializer(insurance, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        insurance.delete()
+        return HttpResponse(status=204)
+    
+@csrf_exempt
+def sacco_detail(request, IDNo_id):
+    """
+    Retrieve, update or delete a sacco.
+    """
+    try:
+        sacco = Sacco.objects.get(IDNo_id=IDNo_id)
+    except Sacco.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SaccoSerializer(sacco)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SaccoSerializer(sacco, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        sacco.delete()
+        return HttpResponse(status=204)
