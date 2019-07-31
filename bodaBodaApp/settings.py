3a1d0403c1d145f70313415570b592bb7741edce
alby <albertagoya@gmail.com>
2019-07-31 11:56:24 +0300

    Gone  back to hosted

--- bodaBodaApp/settings.py
+++ bodaBodaApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2v&---u_$ndq)oahom*y-oi#19m$bb_=r$et3v!ms&-r=ldk=7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['134.209.148.107']
+ALLOWED_HOSTS = ['134.209.148.107']
 
 
 # Application definition
@@ -75,28 +75,28 @@ WSGI_APPLICATION = 'bodaBodaApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bodaboda2',
-        'USER': 'postgres',
-        'PASSWORD': 'dan@1995',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-           
-        } 
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'bodaboda',
-#         'USER': 'aurasafira',
-#         'PASSWORD': '1234pass',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'bodaboda2',
+#         'USER': 'postgres',
+#         'PASSWORD': 'dan@1995',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+           
+#         } 
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bodaboda',
+        'USER': 'aurasafira',
+        'PASSWORD': '1234pass',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 
