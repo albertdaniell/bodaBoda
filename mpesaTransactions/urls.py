cfb0395c68c962bce15abbdfa652b79b70ac594b
alby <albertagoya@gmail.com>
2019-08-01 16:37:45 +0300

    path('api/payments/<phoneNumber>/', views.payment_detail),

--- mpesaTransactions/urls.py
+++ mpesaTransactions/urls.py
@@ -4,5 +4,5 @@ from mpesaTransactions import views
 
 urlpatterns = [
     path('api/payments/', views.payments),
-    path('snippets/<phoneNumber>/', views.payment_detail),
+    path('api/payments/<phoneNumber>/', views.payment_detail),
 ]
