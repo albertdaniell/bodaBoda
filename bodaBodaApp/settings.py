4833c9dcb1e42428549500821de5c3ba0e810c0c
alby <albertagoya@gmail.com>
2019-08-03 06:08:05 +0300

    All migrations cleared

--- bodaBodaApp/settings.py
+++ bodaBodaApp/settings.py
@@ -76,28 +76,28 @@ WSGI_APPLICATION = 'bodaBodaApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'bodaboda2',
-#         'USER': 'postgres',
-#         'PASSWORD': 'dan@1995',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-           
-#         } 
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bodaboda',
-        'USER': 'aurasafira',
-        'PASSWORD': '1234pass',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bodaboda2',
+        'USER': 'postgres',
+        'PASSWORD': 'dan@1995',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+           
+        } 
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bodaboda',
+#         'USER': 'aurasafira',
+#         'PASSWORD': '1234pass',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 
 
