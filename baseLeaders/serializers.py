18304c6b61634f5285fe14890c58ed6bcae47cf2
alby <albertagoya@gmail.com>
2019-08-03 07:44:35 +0300

    fields=('id','Name','Email','date','phone_number','password')

--- baseLeaders/serializers.py
+++ baseLeaders/serializers.py
@@ -5,4 +5,4 @@ from .models import BaseLeaders
 class LeadersSerializer(serializers.ModelSerializer):
     class Meta:
         model=BaseLeaders
-        fields=('id','Name','Email','date','phone_number')
+        fields=('id','Name','Email','date','phone_number','password')
